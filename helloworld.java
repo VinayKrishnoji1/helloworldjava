3616d1b4461ec9464530c0f34506d0edb65a592b
VinayKrishnoji1 <109950045+VinayKrishnoji1@users.noreply.github.com>
2022-08-21 22:47:56 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - 21th August 2022 - Night 22:41 PM");
+    System.out.println("Hello World - 21th August 2022 - Night 22:47 PM");
     }
 }
